36cf0fa6992fe9ab3181d1bf7b3f286be2eb8d25
Nono0007 <noreenmaagaya24@gmail.com>
2023-06-05 00:37:49 +0200

    appointment form

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ function openCity(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
   }
+
+  var currentDateTime = new Date();
+var year = currentDateTime.getFullYear();
+var month = (currentDateTime.getMonth() + 1);
+var date = (currentDateTime.getDate() + 1);
+
+if(date < 10) {
+  date = '0' + date;
+}
+if(month < 10) {
+  month = '0' + month;
+}
+
+var dateTomorrow = year + "-" + month + "-" + date;
+var appointmentElem = document.querySelector("#appointment-date");
+var appointmentElem = document.querySelector("#appointment-time");
+
+appointmentElem.setAttribute("min", dateTomorrow);
+
+appointmentElem.onchange = function () {
+    appointmentElem.setAttribute("min", this.value);
+}
